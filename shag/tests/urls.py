644a09d9d91ff5a7b41f4c9cc3d24d12ce6a2d76
Isamutdin <mr.aisamutdin@mail.ru>
2022-12-20 19:05:28 +0300

    question_delete

--- shag/tests/urls.py
+++ shag/tests/urls.py
@@ -6,7 +6,7 @@ from .views import *
 urlpatterns = [
     path('question_create/<int:pk>/', QuestionCreateView.as_view(), name='question_create'),#pk теста
     path('question_update/<int:pk>/<int:id>/', QuestionUpdateView.as_view(), name='question_update'),#pk вопроса, а id теста
-    path('question_delete/<int:pk>/<int:id>/', QuestionDeleteView.as_view(), name='question_delete'),
+    path('question_delete/<int:pk>/<int:id>/', QuestionDeleteView.as_view(), name='question_delete'),#pk вопроса, а id теста
 
     path('test_create/', TestCreateView.as_view(), name='test_create'),
 ]
