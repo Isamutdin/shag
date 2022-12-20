d6dce1e65da23e581e024452f95fccda5919748a
Isamutdin <mr.aisamutdin@mail.ru>
2022-12-20 14:19:52 +0300

    question_update

--- shag/tests/urls.py
+++ shag/tests/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('question_create/<int:pk>/', QuestionCreateView.as_view(), name='question_create'),
+    path('question_create/<int:pk>/', QuestionCreateView.as_view(), name='question_create'),#pk теста
+    path('question_update/<int:pk>/<int:id>/', QuestionUpdateView.as_view(), name='question_update'),#pk вопроса, а id теста
+
     path('test_create/', TestCreateView.as_view(), name='test_create'),
 ]
--- shag/tests/views.py
+++ shag/tests/views.py
@@ -13,10 +13,42 @@ class QuestionCreateView(CreateView):
     form_class = QuestionForm
 
     def get_context_data(self, **kwargs):
-        self.pk = self.kwargs['pk']
+
         data = super().get_context_data(**kwargs)
-        data['test'] = Test.objects.get(pk=self.pk)
-        m2m = TestQuestions_m2m.objects.filter(test_id=self.pk).values_list('question_id')
+        data['test'] = Test.objects.get(pk=self.kwargs['pk'])# через урл передается pk теста
+
+        m2m = TestQuestions_m2m.objects.filter(test_id=self.kwargs['pk']).values_list('question_id')#через таблицу m2m я извлекаю все id вопросов принадлежащие тесту
+        data['questions'] = []
+
+        for el in m2m:
+            data['questions'].append(Question.objects.get(pk=el[0]))#после чего я нахожу по этим id вопросы из модели Question
+
+        return data
+
+    def form_valid(self, form):
+
+        question = Question.objects.create(**form.cleaned_data)#создаю в бд вопрос
+        test_question_m2m = TestQuestions_m2m.objects.create(test_id=self.kwargs['pk'], question_id=question.pk)#создаю привязку вопроса к тесту
+
+        question.save()
+        test_question_m2m.save()
+        return redirect(self.get_success_url())
+        
+
+    def get_success_url(self) -> str:
+        return reverse("question_create", args=[self.kwargs['pk']]) #передаю пк с тестом на туже стрнаицу
+
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    template_name = "question/question_form.html"
+    form_class = QuestionForm
+
+    def get_context_data(self, **kwargs):# тоже самое что и create
+        data = super().get_context_data(**kwargs)
+        data['test'] = Test.objects.get(pk=self.kwargs['id'])
+
+        m2m = TestQuestions_m2m.objects.filter(test_id=self.kwargs['id']).values_list('question_id')
         data['questions'] = []
 
         for el in m2m:
@@ -24,16 +56,8 @@ class QuestionCreateView(CreateView):
 
         return data
 
-    def form_valid(self, form):
-        question = Question.objects.create(**form.cleaned_data)
-        test_question_m2m = TestQuestions_m2m.objects.create(test_id=self.kwargs['pk'], question_id=question.pk)
-        question.save()
-        test_question_m2m.save()
-        return redirect(self.get_success_url())
-        
-
-    def get_success_url(self) -> str:
-        return reverse("question_create", args=[self.kwargs['pk']])
+    def get_success_url(self) -> str:#pk вопроса, а id теста
+        return reverse("question_create", args=[self.kwargs['id']])
 
 
 class TestCreateView(CreateView):
@@ -46,6 +70,7 @@ class TestCreateView(CreateView):
 
     def form_valid(self, form):
         test = Test.objects.create(**form.cleaned_data)
+        test.save()
         return redirect(self.get_success_url(test.pk))
 
 
